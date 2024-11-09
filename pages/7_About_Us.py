52a195fc6b310f521e6ab0b3d72f089e41a230b1
redavis22 <redavis22@gmail.com>
2024-11-09 13:06:14 -0500

    Update 7_About_Us.py

--- pages/7_About_Us.py
+++ pages/7_About_Us.py
@@ -3,8 +3,7 @@ import streamlit as st
 st.title("About Us")
 
 st.subheader("Kenny Howard")
-[LinkedIn Profile](https://www.linkedin.com/in/kenhow1/)
-""", unsafe_allow_html=True)
+st.markdown("[LinkedIn Profile](https://www.linkedin.com/in/kenhow1/)", unsafe_allow_html=True)
 # st.image("path_to_kenny_image.jpg", width=150)  # Replace with the actual path to Kenny's image
 st.write("""
 **Position**: R&D Software Engineer (Intern) with a Data Science focus at Sandia National Laboratories
@@ -36,17 +35,10 @@ st.write("""
 
 ### Extracurricular Activities
 - Member of the AI Club at North Carolina State University (2021)
-
-[LinkedIn Profile](https://www.linkedin.com/in/kenhow1/)
-""", unsafe_allow_html=True)
-
-
-
+""")
 
 st.subheader("Jaylan Hunter")
-[LinkedIn Profile](https://www.linkedin.com/in/jaylan-hunter-2436a2225/)
-""", unsafe_allow_html=True)
-
+st.markdown("[LinkedIn Profile](https://www.linkedin.com/in/jaylan-hunter-2436a2225/)", unsafe_allow_html=True)
 # st.image("path_to_jaylan_image.jpg", width=150)  # Replace with the actual path to Jaylan's image
 st.write("""
 **Position**: Cyber Futures Intern at the MITRE Corporation
@@ -68,4 +60,4 @@ I'm a Computer Science student with a strong interest in Cybersecurity. I enjoy 
 - Strategic Planning, Critical Thinking
 - Project Planning, Cybersecurity, Python Programming
 - Adversary Emulation, AI for Predictive Analytics, Zero Trust Security
-
+""")
